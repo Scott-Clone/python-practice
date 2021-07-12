803b74b7416966a9e34c3ce6bd4e2c501d192787
Carlone Scott <carlonescott@gmail.com>
2021-07-11 22:21:08 -0400

    Create merge_the_tools.py

---
+++ hackerrank-practice/merge_the_tools.py
@@ -0,0 +1,11 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    x = [list(dict.fromkeys(string[i:i+k])) for i in range(0, len(string), k)]
+    
+    for i in x:
+
+        print(''.join(i))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
